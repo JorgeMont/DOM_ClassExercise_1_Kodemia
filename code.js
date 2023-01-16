8c7166da7c9652ba1ac982b488d2c4c5aefe5bb1
AldoMardel <aldomardel@gmail.com>
2023-01-15 22:59:44 -0600

    Propuesta solucion2

--- code.js
+++ code.js
@@ -2,35 +2,42 @@ import api from "./api.js";
 
 // console.log(api[0]);
 
-const infoPersonajes = api.map((personaje) => {
-  const { id, name, status, species, type, gender, image } = personaje;
-  return { id, name, status, species, type, gender, image };
-});
 
-// Referencia al ul del DOM
-const listaPersonajes = document.querySelector('#listaPersonajes');
+const listPersonajes = (api)=>{
+  const personajes = api.map((personaje)=>{
+    const {id,name,status,species,type,gender,image} = personaje;
+    return {id,name,status,species,type,gender,image};
+  });
+  
+  const lista = document.querySelector('#listaPersonajes');
+  
+  const createElementos =  personajes.forEach((item) => {
+    
+    const card = document.createElement('div');
+    const cardImage = document.createElement('img');
+        const cardBody = document.createElement('div');
+        const cardBodyText = document.createElement('p');
+    
+        card.classList.add('card','shadow');
+        cardBody.classList.add('card-body');
+        cardImage.classList.add('card-img-top');
+        cardBodyText.classList.add('cardText', 'fw-semibold');
+            
+        card.appendChild(cardImage);
+        card.appendChild(cardBody);
+        cardBody.appendChild(cardBodyText);
+        lista.appendChild(card);
+              
+        cardImage.src = item.image;
+        cardBodyText.innerHTML += `Id:  ${item.id} </br>`;
+        cardBodyText.innerHTML += `Name:  ${item.name}</br>`;
+        cardBodyText.innerHTML += `Status:  ${item.status}</br>`;
+        cardBodyText.innerHTML += `Species:  ${item.species}</br>`;
+        cardBodyText.innerHTML += `Type:  ${item.type}</br>`;
+        cardBodyText.innerHTML += `Gender: ${item.gender}</br>`; 
+      });
+    return createElementos;
 
+  }
 
-infoPersonajes.forEach((personaje) => {
-    const { id, name, status, species, type, gender, image } = personaje
-  //Crear y agregar un elemento por cada personaje
-  //Creando elementos necesarios
-  const elementoLista = document.createElement("li");
-  const card = document.createElement("div");
-  const cardBody = document.createElement("div");
-  const cardImage = document.createElement("img");
-
-  //Agregando clases a los divs
-  card.classList.add("card");
-  cardBody.classList.add("card-body");
-  cardImage.classList.add("card-img-top");
-
-  //Ordenando los elementos
-  card.appendChild(cardBody);
-  card.appendChild(cardImage);
-  elementoLista.appendChild(card);
-
-  cardBody.textContent = name;
-  cardImage.src = image;
-  listaPersonajes.appendChild(elementoLista);
-});
+  listPersonajes(api);
